42a1099c3dd258f0ac2c9c3c5c9b3f20c0f529aa
Manuel Kalueka <pedrokalueca@gmail.com>
2024-09-28 14:19:34 +0100

    Refactor: criando loader

---
+++ src/renderer/src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader: React.FC = () => {
+  return (
+    <div className="flex  items-center justify-center">
+      <p>Loading...</p>
+    </div>
+  )
+}
