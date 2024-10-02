4ca994224283b9a5fc6fa9503c50a71fc30346d2
Pedro Manuel Kalueka <pedrokalueca@gmail.com>
2024-10-02 13:14:29 +0100

    Criando exemplo de pdf e colocando font -family offlline

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,6 @@ export default {
         shape:
           '0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03)'
       },
-      colors: {
-        primary: '#4F46E5',
-        secondary: '#D97706'
-      },
       backgroundImage: {
         pattern: 'url(@renderer/assets/bg-child.png)',
         center_bg: 'url(@renderer/assets/center-bg.jpg)'
