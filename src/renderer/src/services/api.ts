d30fdffe1cbf6d4ca3b1396f5dc489df2e76de69
Pedro Manuel Kalueka <pedrokalueca@gmail.com>
2024-10-04 19:24:40 +0100

    Refactor: Timeout de 9s nas requisições para a DB

--- src/renderer/src/services/api.ts
+++ src/renderer/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { BASE_URL } from '../my-env'
 const apiMananger = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  timeout: 9000
 })
 
 export default apiMananger
