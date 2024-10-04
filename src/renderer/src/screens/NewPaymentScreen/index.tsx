a52325337d3c8cfd7f82e01571eec8ba4515c2e6
Pedro Manuel Kalueka <pedrokalueca@gmail.com>
2024-10-04 21:06:41 +0100

    feat. tela de pagamento....

--- src/renderer/src/screens/NewPaymentScreen/index.tsx
+++ src/renderer/src/screens/NewPaymentScreen/index.tsx
@@ -1,8 +1,10 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { HeaderMain } from '@renderer/components/HeaderMain'
 import { Sidebar } from '@renderer/components/Sidebar'
+import { useCenter } from '@renderer/contexts/center-context'
+import { searchStudentService } from '@renderer/services/student'
 import { ArrowRight, BookUser } from 'lucide-react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 const schema = yup
@@ -14,18 +16,46 @@ const schema = yup
 type FormData = yup.InferType<typeof schema>
 
 export const NewPaymentScreen: React.FC = () => {
-  const [student, setStudent] = useState<object | null>(null)
-
+  const [results, setResults] = useState<object | null>(null)
+  const { center } = useCenter()
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors }
   } = useForm<FormData>({
     resolver: yupResolver(schema)
   })
 
+  const studentSearch = watch('studentSearch')
+
+  // Função para buscar dados da API
+  const fetchResults = async (query: string) => {
+    if (query) {
+      try {
+        const response = await searchStudentService(center?._id, query)
+        setResults(response) // Define os resultados vindos da API
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error)
+        setResults(null)
+      }
+    } else {
+      setResults(null) // Limpa os resultados se o input estiver vazio
+    }
+  }
+
+  // useEffect para monitorar o valor do campo de pesquisa e fazer a busca
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      fetchResults(studentSearch)
+    }, 500) // Adiciona um pequeno debounce (meio segundo)
+
+    return () => clearTimeout(delayDebounceFn) // Limpa o debounce se o usuário continuar digitando
+  }, [studentSearch]) // Reexecuta sempre que o valor de searchQuery mudar
+
   const onSubmit = async (data: FormData): Promise<void> => {
-    console.log(data)
+    //executa ao clicar no botão submit
+    await fetchResults(data.studentSearch)
   }
 
   return (
@@ -42,45 +72,40 @@ export const NewPaymentScreen: React.FC = () => {
               <p>Regularize o Pagamento</p>
             </article>
           </div>
-          {student ? (
-            <p>Serei a Ficha de Pagamento</p>
-          ) : (
-            <section className="flex items-center justify-center">
-              <div className="max-w-3xl w-full px-6 text-center space-y-10">
-                <div className="flex flex-col items-center gap-2">
-                  <p className="text-zinc-300 text-lg">
-                    Pesquisar estudantes para começar o processo de pagamento!
-                  </p>
-                </div>
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                  className="h-16 bg-zinc-900 px-4 rounded-lg flex items-center justify-between shadow-shape gap-3"
-                >
-                  <div className="flex items-center gap-2 flex-1">
-                    <BookUser className="size-8 text-zinc-400" />
-                    <input
-                      type="text"
-                      {...register('studentSearch')}
-                      placeholder="Nome ou código do Aluno"
-                      className="bg-transparent text-lg placeholder-zinc-400 outline-none rounded-md shadow-shape flex-1"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="bg-orange-600 text-orange-100 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-orange-700 transition-all"
-                  >
-                    Continuar
-                    <ArrowRight className="size-5" />
-                  </button>
-                </form>
-                {errors.studentSearch && (
-                  <p className="text-red-400">{errors.studentSearch.message}</p>
-                )}
+          <section className="flex items-center justify-center">
+            <div className="max-w-3xl w-full px-6 text-center space-y-10">
+              <div className="flex flex-col items-center gap-2">
+                <p>Estudante correspondete: {results?.name?.fullName}</p>
               </div>
-            </section>
-          )}
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="h-16 bg-zinc-900 px-4 rounded-lg flex items-center justify-between shadow-shape gap-3"
+              >
+                <div className="flex items-center gap-2 flex-1">
+                  <BookUser className="size-8 text-zinc-400" />
+                  <input
+                    type="text"
+                    {...register('studentSearch')}
+                    placeholder="Nome ou código do Aluno"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none rounded-md shadow-shape flex-1"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-orange-600 text-orange-100 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-orange-700 transition-all"
+                >
+                  Continuar
+                  <ArrowRight className="size-5" />
+                </button>
+              </form>
+              {errors.studentSearch && (
+                <p className="text-red-400">{errors.studentSearch.message}</p>
+              )}
+            </div>
+          </section>
         </div>
       </div>
     </div>
   )
 }
+  
---
+++ src/renderer/src/services/student.ts
@@ -0,0 +1,11 @@
+import apiMananger from './api'
+
+export const searchStudentService = async (centerId: string, query: string) => {
+  try {
+    const { data } = await apiMananger.get(`/students/search/${centerId}?query=${query}`)
+    return data
+  } catch (error) {
+    console.log('Erro ao buscar inscrições: ', error)
+    throw error
+  }
+}
